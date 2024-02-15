6cf323ab9e528e032bf66c8b842fb21afad6624a
Tejaswitallapally <145161105+Tejaswitallapally@users.noreply.github.com>
2024-02-15 17:06:33 -0500

    Add files via upload

---
+++ Paws&Tails/Paws&Tails/5/readme.md
@@ -0,0 +1,15 @@
+PURPOSE:
+    #Our website is dedicated to facilitating the adoption of pets. It serves as an interactive platform where potential pet owners can log in, view available pets, and initiate the adoption process. Our goal is to simplify pet adoption, making it more accessible and efficient for everyone involved.
+
+1. Home Page (home.html):
+Purpose: Serves as the main interface for users to log in and access pet adoption services.
+Bootstrap Components:
+Forms, Buttons, Navigation Links, Containers, Header, Responsive Design.
+2. Sign Up Page (signup.html):
+Purpose: Allows new users to register and join the pet adoption community.
+Bootstrap Components:
+Form Controls, Buttons, Cards, Responsive Grid, Custom Styling.
+3. Forgot Password Page (forgot.html):
+Purpose: Provides a way for users to recover their accounts.
+Bootstrap Components:
+Card Layout, Form Controls, Buttons, Responsive Design, Custom Styling, Navigation Links.
---
+++ Paws&Tails/Paws&Tails/5/scripts.js
@@ -0,0 +1,49 @@
+function validateForm(){
+    var firstName = document.getElementById('firstName').value.trim();
+    var lastName = document.getElementById('lastName').value.trim();
+    var phoneNumber = document.getElementById('phoneNumber').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var password = document.getElementById('password').value;
+
+    if (!/^[a-zA-Z]+$/.test(firstName)) {
+        alert("First name should contain only alphabetical characters.");
+        return false;
+    }
+
+    if (!/^[a-zA-Z]+$/.test(lastName)) {
+        alert("Last name should contain only alphabetical characters.");
+        return false;
+    }
+
+    if (!/^\d+$/.test(phoneNumber)) {
+        alert("Phone number should contain only numerical characters.");
+        return false;
+    }
+
+    if (!validateEmail(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters long.");
+        return false;
+    }
+
+    if (!/[A-Z]/.test(password)) {
+        alert("Password must contain at least one capital letter.");
+        return false;
+    }
+
+    if (!/[@!]/.test(password)) {
+        alert("Password must contain at least one special symbol like @ or !");
+        return false;
+    }
+
+    return true;
+}
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
